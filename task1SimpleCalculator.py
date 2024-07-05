e166f0d9d45d8ba48e14e523c8d03dbcff21cd28
Naveen5796 <cherukurinaveen83@gmail.com>
2024-07-05 18:37:58 +0530

    Add files via upload

---
+++ task1SimpleCalculator.py
@@ -0,0 +1,24 @@
+
+ # simple Calculator in python
+
+operator = "+"
+num_1 = 10     # take 2 input integers like num_1 = 10, num_2 = 10
+num_2 = 15
+
+
+if operator == "+":              # used some mathematic operators and also relation operaters 
+    print(num_1 + num_2)         # by Adding Two numbers.
+
+elif operator == "*":
+    print(num_1 * num_2)              # and also used to decision making conditions like if and else if and ele
+
+elif operator == "-":
+    print(num_1 - num_2)
+
+elif operator == "/":
+    print(num_1 / num_2)
+
+elif operator == "%":
+    print(num_1 % num_2)
+
+# Output
